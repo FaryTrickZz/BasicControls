bab4a8ef6b0300e5150061c69442b4f274603ead
Bernardo aparicio cabot <imojino@hotmail.com>
2020-11-03 20:09:20 +0100

    Cambios

--- app/src/main/java/com/example/basiccontrols/MainActivity.java
+++ app/src/main/java/com/example/basiccontrols/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.basiccontrols;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Load animation + settings
+        Animation myAnimation = AnimationUtils.loadAnimation(this, R.anim.translada_cambiocolor);
+        myAnimation.setRepeatCount(Animation.RESTART);
     }
+
+
 }
